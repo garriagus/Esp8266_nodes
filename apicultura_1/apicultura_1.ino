c26fbefbd5d1a488b0f1e5790d6b861df1d25fde
garriagus <agustin_g08@hotmail.com>
2022-11-01 22:53:55 -0300

    Add Oled ssd1306

--- apicultura_1/apicultura_1.ino
+++ apicultura_1/apicultura_1.ino
@@ -11,7 +11,27 @@
 #include <ESPAsyncWebServer.h>
 #include <Adafruit_Sensor.h>
 #include <DHT.h>
-
+#include <SPI.h>
+#include <Wire.h>
+#include <Adafruit_GFX.h>
+#include <Adafruit_SSD1306.h>
+// Temporizador
+unsigned long marcaTiempoDate = 0;
+unsigned long tiempoRefreshDate = 1000;
+ 
+// Variables almacena tiempo millis
+int dias;
+int horas;
+int minutos;
+int segundos;
+// Cadena para almacenar texto formateado
+char cadenaTiempo[16];
+// Definir constantes
+#define ANCHO_PANTALLA 128 // ancho pantalla OLED
+#define ALTO_PANTALLA 64 // alto pantalla OLED
+ 
+// Objeto de la clase Adafruit_SSD1306
+Adafruit_SSD1306 display(128, 64, &Wire, -1);
 const char* ssid     = "ESP8266-Access-Point";
 const char* password = "123456789";
 
@@ -111,12 +131,93 @@ String processor(const String& var){
   }
   return String();
 }
-
+/*
+  Función que convierte millis() a segundos, minutos, horas y días
+  Almacena la información en variables globales
+*/
+void millisToTiempo(unsigned long valMillis) {
+  // Se obtienen los segundos
+  valMillis = valMillis / 1000;
+ 
+  segundos = valMillis % 60; // se divide entre segundos por minuto y te quedas con el resto
+  minutos = valMillis / 60; // Se convierte a minutos
+  minutos = minutos % 60; // se divide entre minutos por hora y te quedas con el resto
+  horas = (valMillis / 60) / 60; // Se convierte en horas
+  horas = horas % 24; // se divide entre horas al día y te quedas con el resto
+  dias = ((valMillis / 60) / 60) / 24; // Se convierte en días
+#ifdef __DEBUG__
+  Serial.print("Segundos = ");
+  Serial.println(valMillis);
+  Serial.print(dias);
+  Serial.print(":");
+  Serial.print(horas);
+  Serial.print(":");
+  Serial.print(minutos);
+  Serial.print(":");
+  Serial.println(segundos);
+#endif
+}
 void setup(){
   // Serial port for debugging purposes
   Serial.begin(115200);
   dht.begin();
+   // Iniciar pantalla OLED en la dirección 0x3C
+  if (!display.begin(SSD1306_SWITCHCAPVCC, 0x3C)) {
+    #ifdef __DEBUG__
+    Serial.println("No se encuentra la pantalla OLED");
+    #endif
+    while (true);
+  }
+   
+    // Iniciar pantalla OLED en la dirección 0x3C
+  if (!display.begin(SSD1306_SWITCHCAPVCC, 0x3C)) {
+#ifdef __DEBUG__
+    Serial.println("No se encuentra la pantalla OLED");
+#endif
+    while (true);
+  }
+ 
+  // Limpir buffer
+  display.clearDisplay();
+ 
+  // Tamaño del texto
+  display.setTextSize(1);
+  // Color del texto
+  display.setTextColor(SSD1306_WHITE);
+  // Posición del texto
+  display.setCursor(10, 32);
+  //Activar página de código 437
+  display.cp437(true);
+  // Escribir el carácter ¡ dos veces
+  display.write(173);
+  display.write(173);
+  // Escribir texto
+  display.println("Hola mundo!!");
   
+  // Enviar a pantalla
+  display.display();
+  delay(2000);
+ 
+  // Mover texto de izquierda a derecha
+  display.startscrollright(0x00, 0x0F);
+  delay(5000);
+  display.stopscroll();
+  
+  // Mover texto de derecha a izquierda
+  display.startscrollleft(0x00, 0x0F);
+  delay(5000);
+  display.stopscroll();
+ 
+  // Mover texto en diagonal hacia la derecha
+  display.startscrolldiagright(0x00, 0x07);
+  delay(5000);
+  display.stopscroll();
+ 
+  // Mover texto en diagonal hacia la izquierda
+  display.startscrolldiagleft(0x00, 0x07);
+  delay(5000);
+  display.stopscroll();
+ 
   Serial.print("Setting AP (Access Point)…");
   // Remove the password parameter, if you want the AP (Access Point) to be open
   WiFi.softAP(ssid, password);
@@ -144,6 +245,35 @@ void setup(){
 }
  
 void loop(){  
+  // Protección overflow
+  if (millis() < marcaTiempoDate) {
+    marcaTiempoDate = millis();
+  }
+ 
+  // Comprobar is hay que actualizar temperatura
+  if (millis() - marcaTiempoDate >= tiempoRefreshDate)
+  {
+    // Actualizar variables de tiempo
+    millisToTiempo(millis());
+    // Componer cadena con la información del tiempo formateada
+    sprintf(cadenaTiempo, "%02d:%02d:%02d:%02d", dias, horas, minutos, segundos);
+    // Marca de tiempo
+    marcaTiempoDate = millis();
+  }
+ 
+  // Limpiar buffer pantalla
+  display.clearDisplay();
+  // Dibujar línea horizontal
+  display.drawLine(0, 18, display.width(), 18, SSD1306_WHITE);
+ 
+  // Dibujar texto tiempo
+  display.setTextSize(1);
+  display.setTextColor(SSD1306_WHITE);
+  display.setCursor(0, 0);
+  display.println(cadenaTiempo);
+ 
+  // Enviar a pantalla
+  display.display();
   unsigned long currentMillis = millis();
   if (currentMillis - previousMillis >= interval) {
     // save the last time you updated the DHT values
